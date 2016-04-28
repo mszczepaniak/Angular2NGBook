c1d710f395f4ddf49fa9e43209b43df12e510f05
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2016-04-28 20:59:03 +0200

    Hello Components

--- app.ts
+++ app.ts
@@ -1,19 +1,28 @@
 // <reference path="node_modules/angular2/ts/typings/node/node.d.ts" />
 // <reference path="node_modules/angular2/typings/browser.d.ts" />
+
 import {bootstrap} from "angular2/platform/browser";
 import {Component} from "angular2/core";
 
 @Component({
     selector: 'hello-world',
-    template: `
-        <div>
-            Hello world
+    template: 
+    (`<div>
+       <ul>
+            <li *ngFor="#name of names">
+       Hello {{ name }}</li> 
+       </ul>
         </div>
-    `
+    `)
 })
 
 class HelloWorld {
+    names2: string[];
+    names: Array<string>;
     
+    constructor() {
+        this.names = ['Michal', 'Kamil', 'Szczepaniak', 'Conqueror'];
+    } 
 }
 
 bootstrap(HelloWorld);
